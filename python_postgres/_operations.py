124413a09d9fd29209333b79757f118549551354
Vinzenz Klass <vinzenz.klass@ba.valantic.com>
2024-11-18 14:30:40 +0100

    feat: pydantic model support

--- python_postgres/_operations.py
+++ python_postgres/_operations.py
@@ -1,8 +1,10 @@
-from typing import Any
+from typing import Any, Collection
 
 import psycopg
-from psycopg import AsyncCursor
+from psycopg import AsyncCursor, sql
+from psycopg.sql import Composed
 from psycopg_pool import AsyncConnectionPool
+from pydantic import BaseModel
 
 from .exceptions import PGError
 from .types import Params, Query
@@ -14,11 +16,16 @@ async def _exec_query(
     query: Query,
     params: Params,
     is_retry: bool = False,
+    **kwargs,
 ) -> None:
     try:
         if not params:
             await cur.execute(query)
             return
+        if isinstance(params, BaseModel) or (
+            isinstance(params, list) and isinstance(params[0], BaseModel)
+        ):
+            params = __pydantic_param_to_values(params, **kwargs)
         if isinstance(params, list):
             await cur.executemany(query, params)
             return
@@ -32,3 +39,33 @@ async def _exec_query(
 
 async def _results(cur: AsyncCursor) -> list[tuple[Any, ...]] | int:
     return await cur.fetchall() if cur.pgresult and cur.pgresult.ntuples > 0 else cur.rowcount
+
+
+def _pydantic_to_query_and_values(
+    table_name: str, model: BaseModel | list[BaseModel], **kwargs
+) -> tuple[Composed, tuple | list[tuple]]:
+    if isinstance(model, list):
+        fields = model[0].model_dump(**kwargs).keys()
+        return (
+            __to_composed(table_name, fields),
+            [tuple(m.model_dump(**kwargs).values()) for m in model],
+        )
+
+    m = model.model_dump(**kwargs)
+    return __to_composed(table_name, m.keys()), tuple(m.values())
+
+
+def __pydantic_param_to_values(model: BaseModel | list[BaseModel], **kwargs) -> tuple | list[tuple]:
+    return (
+        [tuple(m.model_dump(**kwargs).values()) for m in model]
+        if isinstance(model, list)
+        else tuple(model.model_dump(**kwargs).values())
+    )
+
+
+def __to_composed(table_name: str, fields: Collection[str]) -> Composed:
+    return sql.SQL("INSERT INTO {} ({}) VALUES ({})").format(
+        sql.Identifier(table_name),
+        sql.SQL(",").join(map(sql.Identifier, fields)),
+        sql.SQL(",").join(sql.Placeholder() * len(fields)),
+    )
--- python_postgres/_pg.py
+++ python_postgres/_pg.py
@@ -1,13 +1,13 @@
 import asyncio
-import re
 from contextlib import asynccontextmanager
 from typing import Any, AsyncIterator
 from urllib.parse import quote_plus
 
 import psycopg
 from psycopg_pool import AsyncConnectionPool
+from pydantic import BaseModel
 
-from ._operations import _exec_query, _results
+from ._operations import _exec_query, _pydantic_to_query_and_values, _results
 from ._transactions import Transaction
 from .exceptions import PGError
 from .types import Params, Query
@@ -35,17 +35,21 @@ class Postgres:
         :param pool_max_size: The maximum number of connections to keep in the pool.
         """
         self._uri = f"postgresql://{user}:{quote_plus(password)}@{host}:{port}/{database}"
-        self._param_pattern = re.compile(r"\$\d+")
         self._pool = _con_pool = AsyncConnectionPool(
             self._uri, min_size=pool_min_size, max_size=pool_max_size, open=False
         )
 
-    async def __call__(self, query: Query, params: Params = ()) -> list[tuple[Any, ...]] | int:
+    async def __call__(
+        self, query: Query, params: Params = (), **kwargs
+    ) -> list[tuple[Any, ...]] | int:
         """
         Execute a query and return the results. Check the `psycopg` documentation for more
         information.
         :param query:  The query to execute.
         :param params: The parameters to pass to the query.
+        :param kwargs: Keyword arguments passed to the Pydantic serialization method,
+               such as `by_alias`, `exclude`, etc. This is usually the easiest way to
+               make sure your model fits the table schema definition.
         :return: The results of the query.
         """
         try:
@@ -56,6 +60,23 @@ class Postgres:
                     return await _results(cur)
         except psycopg.Error as error:
             raise PGError from error
+
+    async def write_pydantic(
+        self, table_name: str, model: BaseModel | list[BaseModel], **kwargs
+    ) -> int:
+        """
+        Write a Pydantic model to the database. The model must have the same fields as the table
+        in the database.
+        :param table_name: The name of the table to write to.
+        :param model: The Pydantic model to write to the database.
+        :param kwargs: Keyword arguments passed to the Pydantic serialization method,
+                       such as `by_alias`, `exclude`, etc. This is usually the easiest way to
+                       make sure your model fits the table schema definition.
+        :return: The number of rows affected.
+        """
+        query, values = _pydantic_to_query_and_values(table_name, model, **kwargs)
+        print(query.as_string())
+        return await self.__call__(query, values)
 
     @asynccontextmanager
     async def transaction(self) -> AsyncIterator[Transaction]:
