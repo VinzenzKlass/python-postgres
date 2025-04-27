2d9a214122e41317a7062c8d1e490876a6834d18
Vinzenz Klass <76391770+VinzenzKlass@users.noreply.github.com>
2025-04-27 17:46:50 +0200

    feat: extensions (#39)

--- python_postgres/_client.py
+++ python_postgres/_client.py
@@ -1,5 +1,12 @@
 from contextlib import asynccontextmanager
-from typing import AsyncIterator, LiteralString, Optional, Type, TypeVar, overload
+from typing import (
+    AsyncIterator,
+    LiteralString,
+    Optional,
+    Type,
+    TypeVar,
+    overload,
+)
 from urllib.parse import quote_plus
 
 import psycopg
@@ -9,16 +16,9 @@ from pydantic import BaseModel
 
 from ._operations import exec_query, expand_values
 from ._transactions import Transaction
-from .types import Params, PydanticParams, Query
+from .types import AsyncConnectionPatch, Params, PydanticParams, Query
 
 T = TypeVar("T", bound=BaseModel)
-
-
-async def _configure(con: psycopg.AsyncConnection) -> None:
-    """
-    Configure the Connections in the pool.
-    """
-    await con.set_autocommit(True)
 
 
 class Postgres:
@@ -31,6 +31,7 @@ class Postgres:
         database: str = "postgres",
         pool_min_size: int = 10,
         pool_max_size: int = 50,
+        patch: Optional[AsyncConnectionPatch] = None,
         name: str = "python-postgres",
         timeout: float = 30.0,
         max_waiting: int = 0,
@@ -52,6 +53,8 @@ class Postgres:
         :param database: The database name to connect to, default is `postgres`.
         :param pool_min_size: The minimum number of connections to keep in the pool.
         :param pool_max_size: The maximum number of connections to keep in the pool.
+        :param patch: A list of functions to call on the connection after it is created. This is
+               useful to set up the connection with custom settings, like enabling extensions.
         :param name: An optional name to give to the connection pool, to identify it in the logs.
                      Default to `python-postgres` to distinguish it from other pools that may be
                      active.
@@ -64,13 +67,19 @@ class Postgres:
         :param reconnect_timeout: The timeout in seconds to wait for a connection to be reconnected
                                     from the pool. Default is 5 minutes.
         """
+
+        async def configure(con: psycopg.AsyncConnection) -> None:
+            for func in patch:  # We cannot assume concurrency is safe.
+                await func(con)
+            await con.set_autocommit(True)
+
         self._uri = f"postgresql://{user}:{quote_plus(password)}@{host}:{port}/{database}"
         self._pool = AsyncConnectionPool(
             self._uri,
             min_size=pool_min_size,
             max_size=pool_max_size,
             open=False,
-            configure=_configure,
+            configure=configure,
             name=name,
             timeout=timeout,
             max_waiting=max_waiting,
@@ -89,11 +98,7 @@ class Postgres:
     async def __call__(self, query: Query, params: Params = (), **kwargs) -> list[tuple] | int: ...
 
     async def __call__(
-        self,
-        query: Query,
-        params: Params = (),
-        model: Optional[Type[T]] = None,
-        **kwargs,
+        self, query: Query, params: Params = (), model: Optional[Type[T]] = None, **kwargs
     ) -> list[T] | list[tuple] | int:
         """
         Execute a query and return the results, or the number of affected rows. You can pass any
@@ -124,7 +129,11 @@ class Postgres:
                 )
 
     async def insert(
-        self, table_name: LiteralString, params: PydanticParams, prepare: bool = False, **kwargs
+        self,
+        table_name: LiteralString,
+        params: PydanticParams,
+        prepare: bool = False,
+        **kwargs,
     ) -> int:
         """
         Dynamically expand an insert query to correctly handle pydantic models with optional
